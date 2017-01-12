eaec71d25de4ad7ca1d45c6a837642ab0e230c97
wangfx <wongfx@foxmail.com>
2017-01-12 18:42:02 +0800

    Initial commit

---
+++ src/main/java/org/stephen/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.stephen;
+
+/**
+ * Created by wangfx on 2017/1/12.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello("scala");
+    }
+}
---
+++ src/main/scala/org/stephen/Hello.scala
@@ -0,0 +1,11 @@
+package org.stephen
+
+/**
+  * Created by wangfx on 2017/1/12.
+  */
+class Hello {
+  def sayHello(x: String): Unit = {
+    println("hello," + x);
+  }
+
+}
